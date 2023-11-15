b408a6484cdea456b8e56f53d09c561c12b35f40
Frida 3 <149642064+frida-al@users.noreply.github.com>
2023-11-14 20:54:07 -0600

    main.cpp
    Update 1 on main.cpp. New version adapts to UML and new class

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 * Proyecto Invernadero
 * Frida Arcadia Luna
 * A01711615
-* 09 de noviembre 2023
+* 14 de noviembre 2023
 */
 
 /*
@@ -15,9 +15,7 @@ de agua que necesitan las flores en cada estación del año (input del usuario) 
 //Bibliotecas 
 #include <iostream>
 #include <string>
-#include "Flores.h" // Clase 1: Flores
-#include "Medicinales.h" // Clase 2: Plantas medicinales
-#include "Fruit.h" // Árboles frutales
+#include "Planta.h"
 
 //Función imprime menú
 void menu(){
@@ -34,19 +32,19 @@ int main(){
     bool continua = true; //variable para ciclar el programa
 
     // Objetos clase 1:
-    Flores flor1("red", 2, "Rose bush", 15.0);
-    Flores flor2("white", 3, "Gardenia", 0.016);
-    Flores flor3("purple", 4, "Tulip", 2.0 );
+    Flores flor1("Rose bush", 2, "red", 15.0);
+    Flores flor2("Gardenia", 3, "white", 0.016);
+    Flores flor3("Tulip", 4, "purple", 2.0 );
 
     // Objetos clase 2:
-    Medicinales planta1("Arnica", "Europe", "Anti-inflamatory"); 
-    Medicinales planta2("Peppermint","Western Europe", "Gastrointestinal diseases");
-    Medicinales planta3("Aloe Vera","Africa", "Burns and wounds");
+    Medicinales planta1("Arnica", 1, "Europe", "Anti-inflamatory"); 
+    Medicinales planta2("Peppermint", 2, "Western Europe", "Gastrointestinal diseases");
+    Medicinales planta3("Aloe Vera", 4, "Africa", "Burns and wounds");
 
     //Objetos clase 3:
-    Fruit frutal1("Peach","Tree","July-August", 0); 
-    Fruit frutal2("Orange","Tree","September-April", 0);
-    Fruit frutal3("Cherry","Tree","March-April", 0);
+    Fruit frutal1("Peach", 6, "Tree","July-August", 0); 
+    Fruit frutal2("Orange", 7, "Tree","September-April", 0);
+    Fruit frutal3("Cherry", 5, "Tree","March-April", 0);
 
     //Ciclo para que el sistema siga corriendo mientras no se elija la opción "Exit"
     while (continua == true)
@@ -75,7 +73,7 @@ int main(){
             std::cout << "The flower 3 is " << flor3.get_nombre() << std::endl;
         }
         else if (res1 == 4){
-            std::cout << "Season" << std::endl; 
+            std::cout << "Season: "; 
             std::cin >> temporada;
             flor1.riego(temporada);
             flor2.riego(temporada);
@@ -90,16 +88,19 @@ int main(){
         std::cin >> res2;
         if (res2 == 1){
             std::cout << "The plant is " << planta1.get_nombre() << std::endl;
+            std::cout << "It is " << planta1.get_edad() << "years old" <<std::endl;
             std::cout << "It is from " << planta1.get_origen() << std::endl;
             std::cout << "It is " << planta1.get_uso() << std::endl;
         }
         else if (res2 == 2){
             std::cout << "The plant is " << planta2.get_nombre() << std::endl;
+            std::cout << "It is " << planta1.get_edad() << "years old" <<std::endl;
             std::cout << "It is from " << planta2.get_origen() << std::endl;
             std::cout << "It helps with " << planta2.get_uso() << std::endl;
         }
         else if (res2 == 3){
             std::cout << "The plant is " << planta3.get_nombre() << std::endl;
+            std::cout << "It is " << planta1.get_edad() << "years old" <<std::endl;
             std::cout << "It is from " << planta3.get_origen() << std::endl;
             std::cout << "It helps with " << planta3.get_uso() << std::endl;
         }
@@ -108,26 +109,26 @@ int main(){
         std::cout << "Fruit trees. What do you want to know?" << std::endl <<"1) Fruit plant 1" << std::endl << "2) Fruit plant 2" << std::endl << "3) Fruit plant 3" << std::endl << "4) Height" << std::endl;
         std::cin >> res3;
         if (res3 == 1){
-            std::cout << "The plant is " << frutal1.get_name() << std::endl;
+            std::cout << "The fruit is " << frutal1.get_nombre() << std::endl;
             std::cout << "It is a " << frutal1.get_type() << std::endl;
             std::cout << "It bears fruit on " << frutal1.get_time() << std::endl;
         }
         else if (res3 == 2){
-            std::cout << "The plant is " << frutal2.get_name() << std::endl;
+            std::cout << "The fruit is " << frutal2.get_nombre() << std::endl;
             std::cout << "It is a " << frutal2.get_type() << std::endl;
             std::cout << "It bears fruit on " << frutal2.get_time() << std::endl;
         }
         else if (res3 == 3){
-            std::cout << "The plant is " << frutal3.get_name() << std::endl;
+            std::cout << "The fruit is " << frutal3.get_nombre() << std::endl;
             std::cout << "It is a " << frutal3.get_type() << std::endl;
             std::cout << "It bears fruit on " << frutal3.get_time() << std::endl;
         }
         else if (res3 == 4){
             std::cout << "Age of the tree: "; 
             std::cin >> edad;
-            frutal1.height(edad, 20);
-            frutal2.height(edad, 18);
-            frutal3.height(edad, 12);
+            frutal1.height(20);
+            frutal2.height(18);
+            frutal3.height(12);
             std::cout << "The Peach tree is " << frutal1.get_altura() << " inches tall" << std::endl;
             std::cout << "The Orange tree is " << frutal2.get_altura() << " inches tall" << std::endl;
             std::cout << "The Cherry tree is " << frutal3.get_altura() << " inches tall" << std::endl;
@@ -139,4 +140,4 @@ int main(){
     }
     }
             return 0;
-}            
+}    
