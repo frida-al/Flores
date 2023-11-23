7adcb47d3fa1dd35376cc8a6527d7b13623037b7
Frida 3 <149642064+frida-al@users.noreply.github.com>
2023-11-22 20:34:57 -0600

    Create Abejas.h

---
+++ Abejas.h
@@ -0,0 +1,32 @@
+/*
+* Proyecto Invernadero
+* Frida Arcadia Luna
+* A01711615
+* 22 de noviembre 2023
+*/
+
+#include <string>
+
+// Clase Abejas (composición de clase Flores)
+class Abejas {
+
+    private:
+        std::string abejas;
+
+    public:
+        Abejas(): abejas(""){};
+        Abejas(std::string abe): abejas(abe){};
+        void set_abejas(std::string);
+        bool get_abejas();
+};
+ void Abejas::set_abejas(std::string abe){
+    abejas = abe;
+ }
+bool Abejas::get_abejas(){
+    if (abejas == "Yes"){
+        return true;
+    }
+    else if (abejas == "No"){
+        return false;
+    }
+}
