9ffb9297f2a59ec0186c90f281a0fc40f2a5829a
Frida 3 <149642064+frida-al@users.noreply.github.com>
2023-11-29 14:38:04 -0600

    Update Abejas.h

--- Abejas.h
+++ Abejas.h
@@ -2,28 +2,59 @@
 * Proyecto Invernadero
 * Frida Arcadia Luna
 * A01711615
-* 22 de noviembre 2023
+* 30 de noviembre 2023
+*/
+/**
+ * Clase Abejas (composición de clase Flores)
 */
 
 #ifndef ABEJAS_H
 #define ABEJAS_H
 #include <string>
 
-// Clase Abejas (composición de clase Flores)
+
 class Abejas {
 
     private:
-        std::string abejas;
+    //Declara la variable de instancia
+        std::string abejas; 
 
     public:
-        Abejas(): abejas(""){};
-        Abejas(std::string abe): abejas(abe){};
+     /**
+     *  Constructor por default 
+     * 
+     * @param
+     * @return Objeto Planta
+     */
+        Abejas(): abejas(""){}; 
+    /**
+     *  Constructor 
+     * 
+     * @param nom, ed
+     * @return Objeto Planta
+     */
+        Abejas(std::string abe): abejas(abe){}; // Constructor
+        // Métodos miembros de la clase
         void set_abejas(std::string);
         bool get_abejas();
 };
- void Abejas::set_abejas(std::string abe){
+/**
+ * setter de variable abejas
+ * Asigna a la variable un string 
+ * 
+ * @param abe
+ * @return
+ */
+ void Abejas::set_abejas(std::string abe){ //setter de 
     abejas = abe;
- }
+}
+ /**
+ * getter de variable abejas
+ * regresa verdadero o falso
+ * 
+ * @param
+ * @return booleano
+*/
 bool Abejas::get_abejas(){
     if (abejas == "Yes"){
         return true;
@@ -33,4 +64,4 @@ bool Abejas::get_abejas(){
     }
     return false;
 } 
-#endif
+#endif //ABEJAS_H
